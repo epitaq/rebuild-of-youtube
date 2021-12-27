a92e73f2ca617677d991e4c0bad9bc936db1d4c1
epitaq <somaok2004@gmail.com>
2021-12-27 16:04:39 +0900

    :)

--- youtube_auto_skip.js
+++ youtube_auto_skip.js
@@ -1,35 +1,40 @@
-const host_url = location.host;
-
+let host_url = location.host;
+let watch_id = ''
 if (host_url === "www.youtube.com") {
     setInterval(
         function () {
-            const pathname_url = location.pathname;
-            //console.log(pathname_url);
+            let q = location.search;
+            console.log('q:', q);
+            let pathname_url = location.pathname;
             if (pathname_url.match("/watch")) {
-                //console.log("あるよ〜〜〜");
-                //動画を追従させる
-                try {
-                    const winsize = (window.innerHeight) + "px";
-                    const primary = document.getElementById("primary-inner");
-                    const player = document.getElementById("player");
-                    const second = document.getElementById("secondary-inner");
-                    const bod = document.getElementsByTagName("body");
-                    primary.style.height = winsize;
-                    primary.style.overflowY = "auto";
-                    player.style.position = "sticky";
-                    player.style.top = "0";
-                    player.style.zIndex = "1000";
-                    second.style.height = winsize;
-                    second.style.overflowY = "auto";
-                    //bod[0].style.overflowY = "hidden";
-                } catch (e) {
-                    console.log("e:",e.message);
+                if (watch_id == q) {
+                    console.log('if', q);
+                } else {
+                    console.log('else', q);
+                    watch_id = q;
+                    //動画を追従させる
+                    try {
+                        let winsize = (window.innerHeight-56) + "px";
+                        let primary = document.getElementById("primary-inner");
+                        let player = document.getElementById("player");
+                        let second = document.getElementById("secondary-inner");
+                        let bod = document.getElementsByTagName("body");
+                        primary.style.height = winsize;
+                        primary.style.overflowY = "auto";
+                        player.style.position = "sticky";
+                        player.style.top = "0";
+                        player.style.zIndex = "1000";
+                        second.style.height = winsize;
+                        second.style.overflowY = "auto";
+                        bod[0].style.overflowY = "hidden";
+                    } catch (e) {
+                        console.log("e:",e.message);
+                    }
                 }
             } else {
                 //console.log("ないよ〜〜〜");
             }
         }, 1000
-
     )
     setInterval(
         function () {
