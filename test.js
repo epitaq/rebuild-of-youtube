c27efe532f611e40b436fb211b66ccec37cfdfc3
epitaq <somaok2004@gmail.com>
2021-12-28 22:47:24 +0900

    テスト中

--- test.js
+++ test.js
@@ -13,6 +13,24 @@ if (host_url === "www.youtube.com") {
                     console.log('else', q);
                     watch_id = q;
                     //動画を追従させる
+                    try {
+                        let winsize = (window.innerHeight-56) + "px";
+                        let primary = document.getElementById("primary-inner");
+                        let player = document.getElementById("player");
+                        let second = document.getElementById("secondary-inner");
+                        let bod = document.getElementsByTagName("body");
+                        primary.style.height = winsize;
+                        primary.style.overflowY = "auto";
+                        player.style.position = "sticky";
+                        player.style.top = "0";
+                        player.style.zIndex = "1000";
+                        second.style.height = winsize;
+                        second.style.overflowY = "auto";
+                        //bod[0].style.overflowY = "hidden";
+                        bod[0].style.cssText = css_bod + "overflow-y: hidden !important;"
+                    } catch (e) {
+                        console.log("e:",e.message);
+                    }
                 }
             } else {
                 console.log("ないよ〜〜〜");
